fb5889e6ae89c76c9f71931b3a7a0635b9a60eef
f01061388@gamil.com <f01061388@gamil.com>
2025-12-08 14:56:46 +0200

    adding validation to login

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,6 +1,8 @@
 const passdiv = document.querySelector(".pass");
 const password = document.getElementById("password");
 const passIcon = document.getElementById("passIcon");
+const username = document.getElementById("username");
+const submbuttom = document.getElementById("submit-button");
 
 // focus on password field
 password.onfocus = function () {
@@ -18,5 +20,29 @@ passIcon.onclick = function () {
   } else {
     passIcon.classList.replace("fa-eye", "fa-eye-slash");
     password.type = "password";
+  }
+};
+
+submbuttom.onclick = function (e) {
+  e.preventDefault();
+
+  if (
+    username.value === "" ||
+    password.value === "" ||
+    username.value !== "admin" ||
+    password.value !== "admin"
+  ) {
+    username.style.border = "1px solid red";
+    passdiv.style.border = "1px solid red";
+    document.querySelectorAll(".error")[1].style.display = "block";
+    document.querySelectorAll(".error")[0].style.display = "block";
+
+    return;
+  } else if (username.value === "admin" && password.value === "admin") {
+    username.style.border = "1px solid green";
+    passdiv.style.border = "1px solid green";
+    document.querySelectorAll(".error")[1].style.display = "none";
+    document.querySelectorAll(".error")[0].style.display = "none";
+    window.location.href = "index.html";
   }
 };
